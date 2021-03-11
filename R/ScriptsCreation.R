1a333a328ed6687d9929e582762e961b864b67aa
spholmes <sp.holmes@gmail.com>
2021-03-11 12:35:35 -0800

    Tutorial on graphs, version 0.0.9.

---
+++ R/ScriptsCreation.R
@@ -0,0 +1,21 @@
+usethis::use_tutorial("BasicGraphs", 
+                      "Basics about Graphs", open = interactive())
+usethis::use_tutorial("LayoutofGraphs", 
+                      "Layout for Graphs", open = interactive())
+usethis::use_tutorial("CreatingGraphs", 
+                      "Creating Graphs from Data", open = interactive())
+usethis::use_tutorial("UsingGraphs", 
+                      "Using Graphs for Statistics", open = interactive())
+usethis::use_tutorial("CooccurrenceGraphs", 
+                      "CooccurrenceGraphs", open = interactive())
+
+
+usethis::use_package("igraph")
+usethis::use_package("tidygraph")
+usethis::use_package("ggraph")
+usethis::use_package("phyloseq")
+usethis::use_package("ggplot2")
+usethis::use_package("rworldmap")
+usethis::use_package("ape")
+usethis::use_package("ggnetwork")
+usethis::use_package("phyloseqGraphTest")
